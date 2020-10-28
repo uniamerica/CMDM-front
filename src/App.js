1e8007a490f5c7685862a0f8816a6a0bd6e69a3f
Eric Scardo <eric.sacardo00@gmail.com>
2020-10-28 19:33:12 -0300

    itens sem bootstrap

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <Nav />
+      <Social />
     </div>
 
    
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,35 +7,31 @@ import '../styles/Menu.css';
 function Nav() {
   return (
     <div>
- <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3">
-    <div class="container">
-        <a class="navbar-brand text-primary" href="index.html">Juntos contra a Violência Doméstica</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+ <nav>
+    <div>
+        <a href="index.html">Juntos contra a Violência Doméstica</a>
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Home <span>(Página atual)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Relatos</a>
+                <li>
+                    <a href="#">Relatos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Prucurar Ajuda</a>
+                <li>
+                    <a href="#">Prucurar Ajuda</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contato</a>
+                <li>
+                    <a href="#">Contato</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">O que é violência doméstica?</a>
+                <li>
+                    <a href="#">O que é violência doméstica?</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">(HOME)</a>
+                <li>
+                    <a href="#">(HOME)</a>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Social</a>
+                <li>
+                    <a href="#">Social</a>
                 </li>
             </ul>
         </div>
