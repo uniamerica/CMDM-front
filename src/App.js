776dff1f4adcaf0f5bc15860905c5f22930cabc5
Gabriela Guimarães <gabrielaguimaraes.09@hotmail.com>
2020-11-03 22:40:25 -0300

    Ajustando pastas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
-<<<<<<< HEAD
-import Home from './components/Home/Img_home.jsx'
-=======
-import Social from './pages/Social.jsx'
 import Home from '../src/pages/Home'
->>>>>>> 66a46ac2b8a7c0da4d2eabd7785d5658ee3899f3
 
 
 
@@ -17,6 +12,7 @@ function App() {
     <div>
       <Header />
       <Home/>
+      <Footer />
     </div>
   );
 } 
--- src/components/Header/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import './styles.css';
-
-const header = () => (<header id="main-header">JSHunt</header>);
-export default header;
