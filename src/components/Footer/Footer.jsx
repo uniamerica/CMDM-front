907c03e449ddf1f9c929ef710573ed0ecf7fc6ae
Gabriela Guimarães <gabrielaguimaraes.09@hotmail.com>
2020-11-10 20:51:12 -0300

    adicionando redes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,17 @@ function Footer(){
             <div className="redes">
                 <p2><strong>Conecte-se conosco em nossas redes sociais!</strong></p2>
             </div>
+            <section>
+                <div className="rede" id="facebook">
+                <img src={require("../../img/face.jpg")}></img>
+                </div>
+                <div className="rede" id="twitter">
+                <img src={require("../../img/insta.png")}></img>
+                </div>
+                <div className="rede" id="instagram">
+                <img src={require("../../img/twitter.png")}></img>
+                </div>
+            </section>
             <div className="containers">
             <div className= "container">
                   <h5>COMO PROCURAR AJUDA?</h5>
