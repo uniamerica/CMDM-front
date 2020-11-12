ace5ddb54f7bfad1822dc6b7df932a72709a86af
Eric Scardo <eric.sacardo00@gmail.com>
2020-11-12 19:30:34 -0300

    menor textarea

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import { BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Social from '../src/pages/Social.jsx'
 import Ajuda from './components/Ajuda/Ajuda';
 import Home from './pages/Home';
-import Social from './pages/Social'
+import Social from './pages/Social';
 
 
 
@@ -14,11 +13,11 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Switch>
+      <Switch>
           <Route path="/" exact={true} component={Home}/>
-          <Route path="/Home" exact={true} component={Home}/>
-          <Route path="/social" exact component={Social}/>
-        </Switch>
+          <Route path="/Ajuda" exact={true} component={Ajuda}/>
+          <Route path="/Social" exact component={Social}/>
+       </Switch>
       </BrowserRouter>
   
     </div>
