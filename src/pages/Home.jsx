1cee465a0cdc2bc3b87f0beb30fdad45cb0ddd84
Gabriela Guimarães <gabrielaguimaraes.09@hotmail.com>
2020-11-18 19:35:17 -0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Animacao from '../components/Home/Animacao.jsx'
 import ImgHome from '../components/Home/ImgHome.jsx'
 import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer.jsx'
+
 
 function Home(){
     return(
         <div>
           <Header/>
-          <ImgHome/>
-          <Footer/>
+          <ImgHome/>      
         </div>
     );
 }
