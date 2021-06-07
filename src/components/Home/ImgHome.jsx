d2522be06a28338de4b23908cba357dc439d3c70
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-06-07 19:38:16 -0300

    home img change

--- src/components/Home/ImgHome.jsx
+++ src/components/Home/ImgHome.jsx
@@ -6,7 +6,7 @@ import '../Home/ImgHome.css';
 function ImgHome(){
     return(
         <div className="home_img">
-            <img src={require("../../img/img1.jpg")} alt="Home page imagem"></img>
+            <img src={require("../../img/HomeBackground.png")} alt="Home page imagem"></img>
             <div className="effect_phase">
                 <a>Defenda sua vida, lute por sua independência, busque sua felicidade e aprenda a se amar!</a>
             </div>
