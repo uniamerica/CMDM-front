a11e6b03ba7c74e5a934325436b4e7a49024fbe7
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-11-03 17:43:49 -0300

    integration to API and adjusts on CSS

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -2,82 +2,89 @@ import React, { useState } from 'react'
 import Header from '../components/Header/Header'
 
 import styles from '../styles/Cadastro.module.css'
+import api from '../api/api'
+import Titulo from '../components/Relatos/Titulo'
 
 export default function Cadstro() {
-    const [user, setUser] = useState({})
+    const [newUser, setNewUser] = useState({})
 
     function handleChangeNome(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             name: props.target.value
         })
     }
     
     function handleChangeEmail(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             email: props.target.value
         })
     }
     
     function handleChangePhone(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             phone: props.target.value
         })
     }
     
     function handleChangeBirth(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             birth: props.target.value
         })
     }
     
     function handleChangeUsername(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             username: props.target.value
         })
     }
     
     function handleChangePassword(props) {
-        setUser({...user,
+        setNewUser({...newUser,
             password: props.target.value
         })
     }
 
     function handleSubmit(event) {
-        console.log(user)
-
-        event.preventDefault();
+        api.post('/users', newUser)
     }
 
     return (
         <>
             <Header />
+            {/* <Titulo
+                titulo="Registre-se"
+                text="Faça seu cadastro para ter acesso a todas as funcionalidades da plataforma."
+            /> */}
             <div className={styles.formCadastro}>
                 <form onSubmit={handleSubmit}>
                     <div className={styles.formField}>
                         <label>Nome completo</label>
-                        <input id="name" type="text" name="name" placeholder="Maria da Penha" value={user.name} onChange={handleChangeNome}></input>
+                        <input id="name" type="text" name="name" placeholder="Maria da Penha" value={newUser.name} onChange={handleChangeNome}></input>
                     </div>
                     <div className={styles.formField}>
                         <label>Email</label>
-                        <input id="mail" type="text" name="mail" placeholder="maria@email.com" value={user.email} onChange={handleChangeEmail}></input>
+                        <input id="mail" type="text" name="mail" placeholder="maria@email.com" value={newUser.email} onChange={handleChangeEmail}></input>
                     </div>
                     <div className={styles.formField}>
                         <label>Telefone</label>
-                        <input id="phone" type="text" name="phone" placeholder="(DDD) 99999-0000" value={user.phone} onChange={handleChangePhone}></input>
+                        <input id="phone" type="text" name="phone" placeholder="(DDD) 99999-0000" value={newUser.phone} onChange={handleChangePhone}></input>
                     </div>
                     <div className={styles.formField}>
                         <label>Data de nascimento</label>
-                        <input id="birth" type="text" name="bith" placeholder="01/01/1990" value={user.birth} onChange={handleChangeBirth}></input>
+                        <input id="birth" type="date" name="birth" placeholder="01/01/1990" value={newUser.birth} onChange={handleChangeBirth}></input>
                     </div>
                     <div className={styles.formField}>
                         <label>Nome de usuário</label>
-                        <input id="username" type="text" name="username" placeholder="MariaPenha123" value={user.username} onChange={handleChangeUsername}></input>
+                        <input id="username" type="text" name="username" placeholder="MariaPenha123" value={newUser.username} onChange={handleChangeUsername}></input>
                     </div>
                     <div className={styles.formField}>
                         <label>Senha</label>
-                        <input id="password" type="text" name="password" placeholder="************" value={user.password} onChange={handleChangePassword}></input>
+                        <input id="password" type="text" name="password" placeholder="************" value={newUser.password} onChange={handleChangePassword}></input>
                     </div>
                     <button id="cadastroSubmit" type="submit" className={styles.submit}>Cadastrar-se</button>
+                    <a href="/login">
+                        Já tem cadastro?
+                    </a>
                 </form>
             </div>
         </>
