6bfa0834f31447a49cfb1697e36e4abab942157f
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-11-23 19:14:57 -0300

    iniciando testes

--- src/test/Cadastro.test.js
+++ src/test/Cadastro.test.js
@@ -1,29 +1,21 @@
 import React from "react"
-import { render, screen } from '@testing-library/react'
-import { shallow, mount } from 'enzyme'
+import { shallow, } from 'enzyme'
 import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
 import Cadastro from '../pages/Cadastro'
 
-describe("Cadastro Component", () => {
-    test("Should have submit button", () => {
-        render(<Cadastro />)
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
 
-        const submitButton = screen.getByRole("button")
+Enzyme.configure({ adapter: new Adapter() });
 
-        expect(submitButton).toBeInTheDocument();
+describe("Testing Cadastro Component", () => {
+    it("should render correctly", () => {
+        const wrapper = shallow(
+            <Cadastro />
+        )
+
+        expect(wrapper).toMatchSnapshot();
     })
     
-    test("should call method handleSubmit when click on the submit button", () => {
-        render(<Cadastro />)
-        const spy = jest.spyOn(Cadastro.prototype, "handleSubmit")
-
-        const submitButton = screen.getByRole("button")
-        userEvent.click(submitButton)
-
-        expect(spy).toHaveBeenCalled()
-
-        spy.mockRestore()
-    })
 })
