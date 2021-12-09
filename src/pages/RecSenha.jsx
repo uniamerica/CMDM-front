a5a1f06125bcbe475810996c76c39dc55440b4aa
EricSacardo <eric.sacardo00@gmail.com>
2021-12-09 20:44:44 -0300

    recSenha enviando email

--- src/pages/RecSenha.jsx
+++ src/pages/RecSenha.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Titulo from '../components/Relatos/Titulo';
 import styles from '../styles/RecSenha.module.css';
+import emailjs from'emailjs-com';
 
 function Login(){
+    function handleSubmit(event) {
+        const USER_ID = 'user_R6Fr3hkp10QVd9Hyrl9OP';
+        const TEMPLATE_ID= 'template_g3ezq59';
+        const SERVICE_ID= 'cmdmProjeto';
+        event.preventDefault(); 
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, event.target, USER_ID)
+        .then((result) => {
+            alert("Mensagem enviada a Central CMDM!", result.text);
+            window.location.reload();
+        },
+        (error) => {
+            alert("ERRO! ", error.text);
+        });
+    }
     return(
         <div className="recuperar">
             <Header />
             <center><Titulo titulo="Recuperar Senha"/></center>
             <div className="recSenha">
-                <form method="post">
+                <form onSubmit={handleSubmit()}>
                     <input name="usuario" type="email" placeholder="email@email.com" required/>
                     <br/>
                     <span className="span"><i> *Um email com sua nova senha será enviado</i></span>
